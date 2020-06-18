4eba8c87a6248043d85931e4140841fdea630841
Ashley E <poweroftheforce@gmsil.com>
2020-06-18 07:50:57 -0500

    Added bootstrap and initial ui layout

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">display</div>
+        </div>
+        <div className="row">
+          <div className="col-3">AC</div>
+          <div className="col-3">+/-</div>
+          <div className="col-3">%</div>
+          <div className="col-3">&#xF7;</div>
+        </div>
+        <div className="row">
+          <div className="col-3">7</div>
+          <div className="col-3">8</div>
+          <div className="col-3">9</div>
+          <div className="col-3">*</div>
+        </div>
+        <div className="row">
+          <div className="col-3">4</div>
+          <div className="col-3">5</div>
+          <div className="col-3">6</div>
+          <div className="col-3">-</div>
+        </div>
+        <div className="row">
+          <div className="col-3">1</div>
+          <div className="col-3">2</div>
+          <div className="col-3">3</div>
+          <div className="col-3">+</div>
+        </div>
+        <div className="row">
+          <div className="col-6">0</div>
+          <div className="col-3">.</div>
+          <div className="col-3">=</div>
+        </div>
+      </div>
     </div>
   );
 }
