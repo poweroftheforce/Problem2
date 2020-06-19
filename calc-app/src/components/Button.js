493c8b8c9dfa10cd945e796855d6e7019ac86506
Ashley E <poweroftheforce@gmsil.com>
2020-06-19 07:40:38 -0500

    Added alternate skin

--- calc-app/src/components/Button.js
+++ calc-app/src/components/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Button(props) {
   return (
-    <div className={'button col-' + props.size + (props.classNames && props.classNames.length ? ' ' + props.classNames.join(' ') : '')} onClick={props.handler} data-value={props.value}>
-      <div>
-        {props.text ? props.text : props.value}
+    <div className={'col-' + props.size}>
+      <div className={'button ' + (props.classNames && props.classNames.length ? ' ' + props.classNames.join(' ') : '')} onClick={props.handler} data-value={props.value}>
+        <p>{props.text ? props.text : props.value}</p>
       </div>
     </div>
   );
